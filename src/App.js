dd9e440abc347cc67b205aaafd3ba6758833dd58
Kaila Burgin <kailaburgin@gmail.com>
2024-02-20 22:11:36 -0500

    added top level Game component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function Square( {value, onSquareClick} ) {
   );
 }
 
-export default function Board() {
-const [xIsNext, setXIsNext] = useState(true);
-const [squares, setSquares] = useState([]);
+function Board({ xIsNext, squares, onPlay }) {
 
 function handleClick(i) {
   if (squares[i] || calculateWinner(squares)) {
@@ -22,8 +20,7 @@ function handleClick(i) {
   } else {
 nextSquares[i] = 'O';
   }
-  setSquares(nextSquares);
-  setXIsNext(!xIsNext);
+  onPlay(nextSquares);
 }
 
 const winner = calculateWinner(squares);
@@ -54,6 +51,29 @@ status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
       </div>
     </>
   );
+}
+
+export default function Game() {
+const [xIsNext, setXIsNext] = useState(true);
+const [history, setHistory] = useState([]);
+const currentSquares = history[history.length - 1];
+
+function handlePlay(nextSquares) {
+  setHistory([...history, nextSquares]);
+  setXIsNext(!xIsNext);
+}
+
+  return    (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>
+        </ol>
+      </div>
+    </div>
+    );
 }
 
 function calculateWinner(squares) {
